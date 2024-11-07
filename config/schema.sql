85b5ae00afdc08135f7420a0b7ad85d6847fbbe0
Bhuvan Arnaud <bhuvan.arnaud@epitech.eu>
2024-11-07 17:41:17 +0100

    [ADD] new vars in `user` table

--- config/schema.sql
+++ config/schema.sql
@@ -2,6 +2,10 @@ CREATE TABLE IF NOT EXISTS "user"
 (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255),
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    verified BOOLEAN DEFAULT FALSE,
+    verification_code VARCHAR(6),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
