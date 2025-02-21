86d5248f3293a1437eb378445ba53cd35a364f0a
Bhuvan Arnaud <bhuvan.arnaud@epitech.eu>
2025-02-21 22:34:10 +0100

    [ADD] update database schema to include user, workspace, board, list, card, comment, and starred_boards tables

--- config/schema.sql
+++ config/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "user"
 (
-    id SERIAL PRIMARY KEY,
+    id VARCHAR(6) PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255),
     name VARCHAR(255),
@@ -8,4 +8,60 @@ CREATE TABLE IF NOT EXISTS "user"
     verification_code VARCHAR(6),
     picture VARCHAR(2000) DEFAULT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "workspace"
+(
+    id VARCHAR(6) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    owner_id VARCHAR(6) REFERENCES "user"(id),
+    users VARCHAR(6)[] DEFAULT '{}'::VARCHAR(6)[],
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "board"
+(
+    id VARCHAR(6) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    visibility VARCHAR(10) NOT NULL,
+    workspace_id VARCHAR(6) REFERENCES "workspace"(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "list"
+(
+    id VARCHAR(6) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    board_id VARCHAR(6) REFERENCES "board"(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "card"
+(
+    id VARCHAR(6) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    list_id VARCHAR(6) REFERENCES "list"(id),
+    board_id VARCHAR(6) REFERENCES "board"(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    position INTEGER DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS "comment"
+(
+    id VARCHAR(6) PRIMARY KEY,
+    text TEXT NOT NULL,
+    card_id VARCHAR(6) REFERENCES "card"(id),
+    creator_id VARCHAR(6) REFERENCES "user"(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- star/unstar boards
+CREATE TABLE IF NOT EXISTS "starred_boards"
+(
+    user_id VARCHAR(6) REFERENCES "user"(id),
+    board_id VARCHAR(6) REFERENCES "board"(id),
+    PRIMARY KEY (user_id, board_id)
 );
