12f99771dd099150e124c2e14e8d6bf4f89fd30f
Bhuvan Arnaud <bhuvan.arnaud@epitech.eu>
2025-02-21 22:34:44 +0100

    [ADD] integrate unique ID generation into Google signup process

--- server/routes/auth/OAuth/Google.js
+++ server/routes/auth/OAuth/Google.js
@@ -4,14 +4,13 @@ const { logger } = require("../../../config/logger");
 const { jwtDecode } = require('jwt-decode');
 const jwt = require('jsonwebtoken');
 
+const generateRandomId = require('../../../lib/generateId');
+
 async function googleSignup(req, res) {
     const { credential } = req.body;
 
-    const decoded = jwtDecode(credential);
-
-    console.log(decoded);
-
     try {
+        const decoded = jwtDecode(credential);
         const client = db.getDB();
         const user = await client.query('SELECT * FROM "user" WHERE email = $1', [decoded.email]);
 
@@ -19,7 +18,8 @@ async function googleSignup(req, res) {
             return res.status(400).json({ message: 'Email exists already' });
         }
 
-        const newUser = await client.query('INSERT INTO "user" (email, name, password, verified, picture) VALUES ($1, $2, $3, $4, $5) RETURNING *', [decoded.email, decoded.name, null, true, decoded.picture]);
+        const id = await generateRandomId(client, 'user');
+        const newUser = await client.query('INSERT INTO "user" (id, email, name, password, verified, picture) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [id, decoded.email, decoded.name, null, true, decoded.picture]);
 
         if (!newUser || newUser.rows.length === 0) {
             return res.status(500).json({ message: 'Failed to create account' });
@@ -35,11 +35,8 @@ async function googleSignup(req, res) {
 async function googleLogin(req, res) {
     const { credential } = req.body;
 
-    const decoded = jwtDecode(credential);
-
-    console.log(decoded);
-
     try {
+        const decoded = jwtDecode(credential);
         const client = db.getDB();
         const user = await client.query('SELECT * FROM "user" WHERE email = $1', [decoded.email]);
 
@@ -47,7 +44,7 @@ async function googleLogin(req, res) {
             return res.status(401).json({ message: 'User not found' });
         }
 
-        const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user.rows[0].id, email: user.rows[0].email }, process.env.JWT_SECRET, { expiresIn: '1h' });
         return res.status(200).send({ token });
     } catch (error) {
         logger.write(`Error during Google login: ${error.message}`);
