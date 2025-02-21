6bdc8bf3b0984b6dd1220ff62c5a930d00635297
Bhuvan Arnaud <bhuvan.arnaud@epitech.eu>
2025-02-21 22:38:00 +0100

    [UPDATE] refactor Main.css and MainPage component for improved styling and user context handling

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../assets/css/Main.css';
 
+import { AuthContext } from '../auth/AuthContext';
 import Navbar from './Navbar';
+import HomeContainer from './Home';
 
 function Main() {
+  const { user } = useContext(AuthContext);
+
   return (
-    <div className="main-page">
+    <div className={`main-page ${user ? 'main-page-dark' : 'main-page-bright'}`}>
       <Navbar />
-      <h1>EpiTrello</h1>
+      {user && (
+        <HomeContainer infos={user} />
+      )}
+      {!user && (
+        <div className='first-page'>
+
+        </div>
+      )}
+      {/* <h1>EpiTrello</h1>
+      <h2>Welcome, {user ? user.name : 'Guest'}</h2> */}
     </div>
   );
 }
