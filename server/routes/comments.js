f1f9fbdd51025c2e1930f3f112f02d9ce74cfbfd
Bhuvan Arnaud <bhuvan.arnaud@epitech.eu>
2025-02-21 22:36:03 +0100

    [ADD] implement comment management routes with token verification

---
+++ server/routes/comments.js
@@ -0,0 +1,62 @@
+const { db } = require('../config/db');
+const { logger } = require("../config/logger");
+
+const generateRandomId = require('../lib/generateId');
+const check_token = require('../lib/checkToken');
+
+async function createComment(req, res) {
+    const { text, cardId, creatorId } = req.body;
+
+    if (check_token(req, res) !== 200) {
+        return res.status(401).send({ message: 'Unauthorized' });
+    }
+
+    try {
+        const client = db.getDB();
+
+        const id = await generateRandomId(client, 'comment');
+
+        const result = await client.query('INSERT INTO "comment" (id, text, card_id, creator_id) VALUES ($1, $2, $3, $4) RETURNING *', [id, text, cardId, creatorId]);
+        res.status(201).send(result.rows[0]);
+    } catch (err) {
+        logger.write(`Error creating comment: ${err.message}`);
+        logger.log();
+        res.status(500).send({ message: 'Internal server error' });
+    }
+}
+
+async function getComments(req, res) {
+    const cardId = req.params.cardId;
+
+    if (check_token(req, res) !== 200) {
+        return res.status(401).send({ message: 'Unauthorized' });
+    }
+
+    try {
+        const client = db.getDB();
+        const result = await client.query('SELECT * FROM "comment" WHERE card_id = $1', [cardId]);
+        res.status(200).send(result.rows);
+    } catch (err) {
+        logger.write(`Error fetching comments: ${err.message}`);
+        logger.log();
+        res.status(500).send({ message: 'Internal server error' });
+    }
+}
+
+module.exports = {
+    routes: [
+        {
+            method: 'post',
+            path: '/comments',
+            protected: true,
+            body: { "text": "text", "cardId": "text", "creatorId": "text" },
+            callback: createComment
+        },
+        {
+            method: 'get',
+            path: '/cards/:cardId/comments',
+            protected: true,
+            callback: getComments
+        }
+    ]
+}
