c99cde10b4feaa588939ae4ea06db88c8cc71675
Bhuvan Arnaud <bhuvan.arnaud@epitech.eu>
2025-02-21 22:36:22 +0100

    [ADD] implement star board functionality

---
+++ server/routes/star_board.js
@@ -0,0 +1,76 @@
+const { db } = require('../config/db');
+const { logger } = require("../config/logger");
+const check_token = require('../lib/checkToken');
+
+// This function is called when a user stars or unstars a board
+// It adds or removes a row from the 'starred_boards' table
+// The row contains the user_id and board_id
+// If the row already exists, it is removed
+// If the row does not exist, it is added
+// The function returns a 200 status code upon success
+async function starBoard(req, res) {
+    const { boardId, userId } = req.body;
+
+    if (check_token(req, res) !== 200) {
+        return res.status(401).send({ message: 'Unauthorized' });
+    }
+
+    try {
+        const client = db.getDB();
+        const result = await client.query('SELECT * FROM "starred_boards" WHERE user_id = $1 AND board_id = $2', [userId, boardId]);
+
+        if (result.rows.length > 0) {
+            await client.query('DELETE FROM "starred_boards" WHERE user_id = $1 AND board_id = $2', [userId, boardId]);
+        } else {
+            await client.query('INSERT INTO "starred_boards" (user_id, board_id) VALUES ($1, $2)', [userId, boardId]);
+        }
+
+        res.status(200).send({ message: 'OK' });
+    } catch (err) {
+        logger.write(`Error starring board: ${err.message}`);
+        logger.log();
+        res.status(500).send({ message: 'Internal server error' });
+    }
+}
+
+// This function is called when a user wants to view their starred boards
+// It returns an array of board objects that the user has starred
+// The function returns a 200 status code upon success
+async function getStarredBoards(req, res) {
+    const { userId } = req.params;
+
+    if (check_token(req, res) !== 200) {
+        return res.status(401).send({ message: 'Unauthorized' });
+    }
+
+    try {
+        const client = db.getDB();
+        const result = await client.query('SELECT * FROM "starred_boards" WHERE user_id = $1 ', [userId]);
+        res.status(200).send(result.rows);
+    } catch (err) {
+        logger.write(`Error fetching starred boards: ${err.message}`);
+        logger.log();
+        res.status(500).send({ message: 'Internal server error' });
+    }
+}
+
+module.exports = {
+    routes: [
+        {
+            method: 'post',
+            path: '/star-board',
+            protected: true,
+            body: {
+                "boardId": "text",
+                "userId": "text"
+            },
+            callback: starBoard
+        },
+        {
+            method: 'get',
+            path: '/:userId/starred-boards',
+            protected: true,
+            callback: getStarredBoards
+        }
+    ]
+}
